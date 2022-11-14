bd8d35c0c4be2bfd919bdafd1c355655fb1f5a9b
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2022-11-14 21:35:55 +0100

    Added missing dependency for internationalization

--- test/home_screen_test.dart
+++ test/home_screen_test.dart
@@ -1,5 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:notes_app_provider/home/view/home_screen.dart';
+import 'package:notes_app_provider/home/widgets/widgets.dart';
 
 void main() {
-  group('Home Screen Tests', () {});
+  group('Home Screen Tests', () {
+    testWidgets('Correctly opens home screen', (WidgetTester tester) async {
+      await tester.pumpWidget(const HomeScreen());
+      await tester.pump();
+      final notesListFinder = find.byType(NotesListView);
+      expect(notesListFinder, findsOneWidget);
+    });
+  });
 }
